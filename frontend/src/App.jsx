a15f3a9ffdf23f7987e5b258cc7b9f69c824dacc
Long Phan <longnphan@outlook.com>
2023-08-27 15:27:10 -0600

    init commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavbarSimple } from "./components/NavbarSimple";
+
+function App() {
+  return (
+    <>
+      <NavbarSimple />
+    </>
+  );
+}
+
+export default App;
