9a74ded3833ba722764f9c5329cd5f779abd98ae
Long Phan <longnphan@outlook.com>
2023-08-27 18:20:33 -0600

    add NavBar icons and text

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { NavbarSimple } from "./components/NavbarSimple";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <NavbarSimple />
+      <NavBar />
     </>
   );
 }
