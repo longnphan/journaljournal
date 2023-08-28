19e2840fd2a9bc85d38409eeca61f0fd1e6d9b14
Long Phan <longnphan@outlook.com>
2023-08-28 11:30:51 -0600

    add journal entry page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Entry from "./pages/Entry";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { NavBar } from "./components/NavBar";
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/entry" element={<Entry />} />
       </Routes>
     </>
   );
