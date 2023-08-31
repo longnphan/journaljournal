484ea460200b4527c4990f7ed327d207a342ad2f
Long Phan <longnphan@outlook.com>
2023-08-31 16:52:55 -0600

    update journalController, jounralRoutes

--- backend/routes/journalRoutes.js
+++ backend/routes/journalRoutes.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const { admin, protect } = require("../middleware/authMiddleware.js");
+const {
+  createJournal,
+  getJournal,
+  deleteJournal,
+} = require("../controllers/journalController");
 
+router.route("/").post(createJournal).get(getJournal);
+router.route("/:id").delete(protect, deleteJournal);
 
 module.exports = router;
--- frontend/src/pages/Journal.jsx
+++ frontend/src/pages/Journal.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import { Card, CardBody, Typography, Button } from "@material-tailwind/react";
 
 function Journal() {
+  const [journalList, setJournalList] = useState([]);
+
+  const getEntries = async () => {
+    try {
+      const entries = await axios.get("/api/journal");
+      console.log("this is messages inside Journal.jsx:", journalList);
+      setJournalList(entries.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getEntries();
+  }, []);
+
   return (
     <>
       <Card className="mt-6 w-1/2 mx-auto">
